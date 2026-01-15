1a403046fdfed22082d689317f8cd27f6391dca8
bellyfisher <527icubley@frhsd.com>
2026-01-15 11:45:15 -0500

    added questions

---
+++ app.js
@@ -0,0 +1,14 @@
+let prefix = getElementByID('q1' + 'q3');
+
+/* firstName
+middleName
+lastName */
+
+let suffix = getElementByID('q4');
+if {
+    
+}
+
+let fullName = prefix + ' ' + suffix;
+
+display(fullName);
